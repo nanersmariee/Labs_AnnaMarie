b3362545d8d0ab24a62e6d77dc6784078b2560b8
nanersmariee <anna.marie19@gmail.com>
2019-11-02 17:51:33 -0700

    Markov Further Study

--- markov-chains/markov.py
+++ markov-chains/markov.py
@@ -1,7 +1,8 @@
 """Generate Markov text from text files."""
 
 from random import choice
-
+import sys
+print(sys.argv)
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -66,10 +67,27 @@ def make_text(chains):
 
     words = []
 
-    start = choice(list(chains.keys()))
-    words.append(start[0])
-    words.append(start[1])
+    # start = choice(list(chains.keys()))
+    # while True:
+    #     if start[0][0] >= "A" and start[0][0] <= "Z":
+    #         words.append(start[0])
+    #         words.append(start[1])
+    #         break
+    #     else:
+    #         start = choice(list(chains.keys()))
+
+    cap_alpha = set(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", 
+        "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
     
+
+    start = choice(list(chains.keys()))
+    while True:
+        if start[0][0] in cap_alpha:
+            words.append(start[0])
+            words.append(start[1])
+            break
+        else:
+            start = choice(list(chains.keys()))
 
     #loop to get keys
     while True:
@@ -88,7 +106,7 @@ def make_text(chains):
     return " ".join(words)
 
 
-input_path = "gettysburg.txt"
+input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
