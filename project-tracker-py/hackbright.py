abf11438b63a1c3e677dbb8d8197eb7f1dd9965d
nanersmariee <anna.marie19@gmail.com>
2019-12-07 12:56:35 -0800

    Project Tracker and Further Study

--- project-tracker-py/hackbright.py
+++ project-tracker-py/hackbright.py
@@ -80,13 +80,51 @@ def get_project_by_title(title):
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+    QUERY = """
+        SELECT grade
+        FROM grades
+        WHERE student_github = :github AND project_title = :title
+    """
 
+    db_cursor = db.session.execute(QUERY, {"github": github, "title": title})
+
+    row = db_cursor.fetchone()
+    # should be one row response
+
+    print('''Project: {}\n 
+        Student Github: {}\n
+        Grade Given: {}
+        '''.format(title, github, row[0]))
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    
+    INSERT = """
+        INSERT INTO grades (student_github, project_title, grade)
+        VALUES (:github, :title, :grade)
+    """
 
+    db_cursor = db.session.execute(INSERT, {"github": github, "title": title, "grade": grade})
+
+    db.session.commit()
+
+    print(github, title, grade, "has been added")
+
+def make_new_project(title, description, max_grade):
+    """Assign a student a grade on an assignment and print a confirmation."""
+    
+    INSERT = """
+        INSERT INTO projects (title, description, max_grade)
+        VALUES (:title, :description, :max_grade)
+    """
+
+    db_cursor = db.session.execute(INSERT, {"title": title,
+                                            "description": description,
+                                            "max_grade": max_grade})
+
+    db.session.commit()
+
+    print(f"Project {title} has been added")
 
 def handle_input():
     """Main loop.
@@ -108,12 +146,32 @@ def handle_input():
             get_student_by_github(github)
 
         elif command == "project":
-            project = args[0]
-            get_project_by_title(project)
+            title_words = args[:]
+            title = " ".join(title_words)
+            get_project_by_title(title)
+
+        elif command == "lookup_grade":
+            github = args[0]
+            title_words = args[1:]
+            title = " ".join(title_words)
+            get_grade_by_github_title(github, title)
+
+        elif command == "assign_grade":
+            github = args[0]
+            grade = args[1]
+            title_words = args[1:]
+            title = " ".join(title_words)
+            assign_grade(github, title, grade)
 
         elif command == "new_student":
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
+
+        elif command == "new_project":
+            project_title = args[0]
+            max_grade = args[1]
+            description = args[2:]
+            make_new_project(project_title, description, max_grade)
 
         else:
             if command != "quit":
