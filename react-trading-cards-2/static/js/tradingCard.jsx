6560fca0aeb94761b94aae28e1480947a9272df8
nanersmariee <anna.marie19@gmail.com>
2020-01-11 17:01:26 -0800

    react lab part 2

---
+++ react-trading-cards-2/static/js/tradingCard.jsx
@@ -0,0 +1,81 @@
+var tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  }
+];
+
+class TradingCard extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <p>Name: {this.props.name}</p>
+        <img src={this.props.imgUrl} />
+        <p>Skill: {this.props.skill} </p>
+      </div>
+    );
+  }
+}
+
+class TradingCardContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { cards: [] }; //Set initial value
+    this.updateCards = this.updateCards.bind(this);
+  }
+
+  updateCards(response) {
+      const cards = response.cards;
+      this.setState({ cards: cards });
+  }
+
+  getCardData() {
+    // Get the up to date card data from DB.
+
+    $.get('/cards.json', this.updateCards);
+  }
+
+  componentDidMount() {
+    this.getCardData();
+  }
+
+  render() {
+    const tradingCards = [];
+
+    for (const currentCard of this.state.cards){
+      tradingCards.push(
+        <TradingCard
+          key={currentCard.name}
+          name={currentCard.name}
+          skill={currentCard.skill}
+          imgUrl={currentCard.imgUrl}
+        />
+      );
+    }
+
+    return (
+      <div>
+        {tradingCards}
+      </div>
+    );
+  }  
+}
+
+ReactDOM.render(
+  <TradingCardContainer />,
+  document.getElementById('container')
+);
